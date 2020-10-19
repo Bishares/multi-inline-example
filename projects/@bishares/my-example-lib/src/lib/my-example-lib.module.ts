50c50a953379636691d449627c0ed3c0283a7367
Bishares <38129103+Bishares@users.noreply.github.com>
2020-10-19 13:23:35 +0200

    Create my-example-lib.module.ts

---
+++ projects/@bishares/my-example-lib/src/lib/my-example-lib.module.ts
@@ -0,0 +1,49 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LOCALE_ID, NgModule, APP_INITIALIZER, Inject, Injector } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TreeModule } from 'angular-tree-component';
+import { RouterComponent } from './router.component';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+
+export function appInit() {
+  return null;
+}
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatIconModule,
+    MatProgressBarModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    ClipboardModule,
+  ],
+  declarations: [
+    ExampleComponent
+  ],
+  providers: [
+  ],
+  bootstrap: [],
+  exports: []
+})
+export class MyExampleLibModule {}
