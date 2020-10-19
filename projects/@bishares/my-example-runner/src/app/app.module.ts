d5c16a05b9c78cede9a5b42fe48b42dab945e613
Bishares <38129103+Bishares@users.noreply.github.com>
2020-10-19 13:02:51 +0200

    Create app.module.ts

---
+++ projects/@bishares/my-example-runner/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { MyExampleLibModule } from '@bishares/my-example-lib';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MyExampleLibModule,
+  ],
+  providers: [],
+  bootstrap: [RouterComponent]
+})
+export class AppModule { }
